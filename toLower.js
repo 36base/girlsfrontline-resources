2c5421c56168dd025901ac0598f48f3c8e818ec6
CirnoV <cirnov59@gmail.com>
2018-10-15 01:55:34 +0900

    toLowerCase

---
+++ toLower.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.flatMap = function (lambda) {
+  return Array.prototype.concat.apply([], this.map(lambda));
+};
+
+function walkSync(dir) {
+  if (!fs.lstatSync(dir).isDirectory()) {
+    return dir;
+  }
+
+  return fs.readdirSync(dir).flatMap((f) => walkSync(path.join(dir, f)));
+}
+
+walkSync('resources').forEach((dir) => {
+  if (/.atlas$/g.test(dir)) {
+    let atlas = fs.readFileSync(dir).toString();
+    const textureRegex = /.*.png/g;
+    atlas = atlas.replace(textureRegex, textureRegex.exec(atlas)[0].toLowerCase());
+    fs.writeFileSync(dir, atlas);
+  }
+
+  fs.renameSync(dir, dir.toLowerCase());
+});
